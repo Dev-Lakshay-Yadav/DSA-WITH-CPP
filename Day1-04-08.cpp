750915d866673b71c9591dda044665ec3222df00
lakshay <lakshay8219@gmail.com>
2025-08-04 20:14:26 +0530

    Day 1 basic overview and some minimal codes

---
+++ Day1-04-08.cpp
@@ -0,0 +1,84 @@
+// FlowChart
+// -> Basic way of representing logic/code
+// -> way of explaining code logic to a non tech guy
+// -> easy undertanding of code
+// -> includes various shapes like
+// -> oval block -> represent start and end of code
+// -> parallelogram block -> represent input and output
+// -> square block -> represent process like (i=i+1)
+// -> rhombus block -> decison box or logic are like (if case)
+
+// Pseudo Code
+// -> way of writing logic
+// -> simalar to flochart but without any shape just write code in human understandable language
+// -> there is no standard method for pseudo code no boundation of representation
+
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // // Hello world code
+    // cout<<"Hello Worlds!";
+
+    // // odd even
+    // int z=12;
+    // if(z%2==0) cout<<"Even"<<endl;
+    // else cout<<"Odd"<<endl;
+
+    // // given number is positive, negative or zero
+    // int n = 12;
+    // if(n==0) cout<<"Zero"<<endl;
+    // else if(n>0) cout<<"Positive"<<endl;
+    // else cout<<"Negative"<<endl;
+
+    // // valid triangle
+    // int a =4, b=5, c=6;
+    // if((a+b)>c && (b+c) > a && (c+a)>b) cout<<"Yes valid triangle"<<endl;
+    // else cout<<"Not a valid triangle"<<endl;
+
+    // // Loops
+    // // print up to n
+    // int n=5,i=1;
+    // while(i<=n){
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+
+    // // up to n only even  -> for odd make i=1
+    // int n=5,i=2;
+    // while(i<=n){
+    //     cout<<i<<endl;
+    //     i+=2;
+    // }
+
+    // // sum up to n
+    // int n=5, i=1,sum=0;
+    // while(i<=n){
+    //     sum+=i;
+    //     i++;
+    // }
+    // cout<<"sum is : "<<sum<<endl;
+
+    // // factorial of n
+    // int n=5,i=1,ans=1;
+    // while(i<=5){
+    //     ans*=i;
+    //     i++;
+    // }
+    // cout<<"factorial is : "<<ans;
+
+    // // check prime
+    // int n=7,i=2;
+    // bool ans=false;
+    // while(i<n){
+    //     if(n%i==0){
+    //         ans=true;
+    //         break;
+    //     }
+    //     i++;
+    // }
+    // if(ans) cout<<"Not Prime"<<endl;
+    // else cout<<"yes Prime"<<endl;
+    return 0;
+}
