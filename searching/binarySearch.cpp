fabe904cdb8f34a6ea7af57e38496e1500d4817f
lakshay <lakshay8219@gmail.com>
2026-01-30 21:46:00 +0530

    binnary search with recursion

--- searching/binarySearch.cpp
+++ searching/binarySearch.cpp
@@ -26,14 +26,34 @@ int searchElement(vector<int> &arr, int target)
     return -1;
 }
 
-
 // with recursion
+int binarySearchRecursion(vector<int> &arr, int target, int start, int end)
+{
+    if (start > end)
+    {
+        return -1;
+    }
 
+    int mid = start + (end - start) / 2;
+    if (target == arr[mid])
+    {
+        return mid;
+    }
+    if (target > arr[mid])
+    {
+        binarySearchRecursion(arr, target, mid + 1, end);
+    }
+    else
+    {
+        binarySearchRecursion(arr, target, start, mid - 1);
+    }
+}
 
 int main()
 {
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int target = 7;
     cout << searchElement(arr, target);
+    cout << binarySearchRecursion(arr, target, 0, arr.size() - 1);
     return 0;
 }
