e6b06e083d20903259d8591fb1664b824c5e1e0f
Dev-Yakshay-Yadav <lakshay8219@gmail.com>
2026-07-07 17:47:05 +0530

    2 sum

---
+++ 2,3,4Sum/2Sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// hashing
+
+vector<int> Sum(vector<int>arr){
+    int n= arr.size();
+}
+
+int main ()
+{
+    vector<int> arr = {5,2,11,7,15};
+    return 0;
+}
